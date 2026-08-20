58607b99ea35956e420adef3f661e092aff1962a
voidbeskar <khususminikrep62@gmail.com>
2026-08-20 20:01:44 +0700

    Add files via upload
    codero sangat menyenagkan

---
+++ trial.py
@@ -0,0 +1,39 @@
+print("hello world")
+print("Faris")
+print("14")
+
+a = 50
+b = 20
+
+#Ini adalah Comment
+#Tambah
+hasil_tambah = a + b
+print("Hasil dari", a, "+", b, "=", hasil_tambah)
+
+#kurang
+hasil_kurang = a - b
+print("hasil dari", a,"-",b,"= ", hasil_kurang)
+
+#kali a * b
+hasil_kali = a * b
+print("hasil dari", a, "*", b, "=", hasil_kali)
+
+#bagi a / b
+hasil_bagi = a / b
+print("hasil dari" , a, "/", b, "=", hasil_bagi)
+
+#Tipe data
+a =10 #Tipe data Integer
+b =2.5 #Tipe data Float
+c = "Faris" #Tipe data String
+d = [1,2,3,4,5] #Tipe data List
+
+squad_faris = {
+    "assasin":"Fanny",
+    "tank":"Tigreal",
+    "support":"Rafaela",
+    "mage":"Xavier" 
+}
+print(squad_faris["assasin"])
+print(squad_faris["mage"])
+                  
